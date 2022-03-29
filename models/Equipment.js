280b3722bce43fe485b58bdf1438d0ecec981631
Tez28 <courtezcannady@gmail.com>
2022-03-28 21:47:44 -0400

    fix stash

--- models/Equipment.js
+++ models/Equipment.js
@@ -21,7 +21,7 @@ Equipment.init(
       type: DataTypes.STRING,
       allowNull: false
     },
-    // define stock column
+    // define  column
     type: {
       type: DataTypes.INTEGER,
       allowNull: false,
