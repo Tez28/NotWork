a5161edd38431d1194be5408dcc67efeb11b32c9
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-03-30 16:26:30 -0500

    tightens up layout, adds link, adds header to signup page, fixes save issue with login/signup

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
-const { User, Post, Comment, Vote } = require('../../models/');
+const {
+  User,
+  Post,
+  Comment,
+  Vote
+} = require('../../models/');
 
 // get all users
 router.get('/', (req, res) => {
   User.findAll({
-    attributes: { exclude: ['password'] }
-  })
+      attributes: {
+        exclude: ['password']
+      }
+    })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
@@ -15,34 +22,37 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   User.findOne({
-    attributes: { exclude: ['password'] },
-    where: {
-      id: req.params.id
-    },
-    include: [
-      {
-        model: Post,
-        attributes: ['id', 'title', 'post_url', 'created_at']
+      attributes: {
+        exclude: ['password']
       },
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'created_at'],
-        include: {
+      where: {
+        id: req.params.id
+      },
+      include: [{
           model: Post,
-          attributes: ['title']
+          attributes: ['id', 'title', 'post_url', 'created_at']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'created_at'],
+          include: {
+            model: Post,
+            attributes: ['title']
+          }
+        },
+        {
+          model: Post,
+          attributes: ['title'],
+          through: Vote,
+          as: 'voted_posts'
         }
-      },
-      {
-        model: Post,
-        attributes: ['title'],
-        through: Vote,
-        as: 'voted_posts'
-      }
-    ]
-  })
+      ]
+    })
     .then(dbUserData => {
       if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
+        res.status(404).json({
+          message: 'No user found with this id'
+        });
         return;
       }
       res.json(dbUserData);
@@ -55,16 +65,17 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password
-  })
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
     .then(dbUserData => {
       req.session.save(() => {
         req.session.user_id = dbUserData.id;
         req.session.username = dbUserData.username;
+        req.session.email = dbUserData.email;
         req.session.loggedIn = true;
-  
+
         res.json(dbUserData);
       });
     })
@@ -82,23 +93,31 @@ router.post('/login', (req, res) => {
     }
   }).then(dbUserData => {
     if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
+      res.status(400).json({
+        message: 'No user with that email address!'
+      });
       return;
     }
 
     const validPassword = dbUserData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect password!' });
+      res.status(400).json({
+        message: 'Incorrect password!'
+      });
       return;
     }
 
     req.session.save(() => {
       req.session.user_id = dbUserData.id;
       req.session.username = dbUserData.username;
+      req.session.email = dbUserData.email;
       req.session.loggedIn = true;
-  
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
+
+      res.json({
+        user: dbUserData,
+        message: 'You are now logged in!'
+      });
     });
   });
 });
@@ -108,8 +127,7 @@ router.post('/logout', (req, res) => {
     req.session.destroy(() => {
       res.status(204).end();
     });
-  }
-  else {
+  } else {
     res.status(404).end();
   }
 });
@@ -119,14 +137,16 @@ router.put('/:id', (req, res) => {
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
-    individualHooks: true,
-    where: {
-      id: req.params.id
-    }
-  })
+      individualHooks: true,
+      where: {
+        id: req.params.id
+      }
+    })
     .then(dbUserData => {
       if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
+        res.status(404).json({
+          message: 'No user found with this id'
+        });
         return;
       }
       res.json(dbUserData);
@@ -139,13 +159,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   User.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
+      where: {
+        id: req.params.id
+      }
+    })
     .then(dbUserData => {
       if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
+        res.status(404).json({
+          message: 'No user found with this id'
+        });
         return;
       }
       res.json(dbUserData);
@@ -157,5 +179,3 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
-
-    
